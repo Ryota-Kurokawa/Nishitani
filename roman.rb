c00dc54ad8e35c6f3472a3453f535bcac660842f
ryota <kurokawa.ryota.1124@gmail.com>
2024-05-08 16:50:33 +0900

    test

--- roman.rb
+++ roman.rb
@@ -15,13 +15,51 @@
 #   return roman
 # end
 
+# def to_roman(arabic)
+#   roman = ''
+#   divider = 5
+#   char = 'V'
+#   p quo= arabic/divider
+#   roman += char*quo
+#   p resideual = arabic%divider
+#   roman += 'I'*resideual
+#   return roman
+# end
+
 def to_roman(arabic)
   roman = ''
-  divider = 5
-  char = 'V'
-  p quo= arabic/divider
+  [
+    [1000, 'M'],
+    [999, 'CMXCIX'],
+    [961, 'CMLXI'],
+    [900, 'CM'],
+    [732, 'DCCXXXII'],
+    [500, 'D'],
+    [499, 'CDXCIX'],
+    [400, 'CD'],
+    [100, 'C'],
+    [99, 'XCIX'],
+    [97, 'XCVII'],
+    [51, 'LI'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+  ].each do |divider, char|
+    # actual = to_roman(arabic)
+    # p [arabic, expected, actual]
+    quo= arabic/divider
+    roman += char*quo
+    resideual = arabic%divider
+    arabic =resideual
+    # roman += 'I'*resideual
+  end
+  divider, char = 5, 'V'
+  quo= arabic/divider
   roman += char*quo
-  p resideual = arabic%divider
+  resideual = arabic%divider
   roman += 'I'*resideual
   return roman
 end
@@ -29,9 +67,9 @@ end
 def re_to_roman(arabic)
   roman = ''
   divider = 5
-  p quo= arabic/divider
+  quo= arabic/divider
   roman += 'X'*quo
-  p resideual = arabic%5
+  resideual = arabic%5
   roman += 'I'*resideual
   return roman
 end
@@ -43,7 +81,32 @@ end
   # [2, 'II'],
   # [3, 'III'],
   [6, "VI"],
-  [11, 'xI'],
+  [7, "VII"],
+  [11, 'XI'],
+  [9, 'IX'],
+  [40, 'XL'],
+  [90, 'XC'],
+  [100, 'C'],
+  [400, 'CD'],
+  [500, 'D'],
+  [900, 'CM'],
+  [1000, 'M'],
+  [14, 'XIV'],
+  [15, 'XV'],
+  [16, 'XVI'],
+  [19, 'XIX'],
+  [38, 'XXXVIII'],
+  [42, 'XLII'],
+  [49, 'XLIX'],
+  [51, 'LI'],
+  [97, 'XCVII'],
+  [99, 'XCIX'],
+  [439, 'CDXXXIX'],
+  [483, 'CDLXXXIII'],
+  [499, 'CDXCIX'],
+  [732, 'DCCXXXII'],
+  [961, 'CMLXI'],
+  [999, 'CMXCIX'],
 ].each do |arabic, expected|
   actual = to_roman(arabic)
   p [arabic, expected, actual]
